e730a1e8ae593d56dffc0eed7122ab5f60a707cf
linxueyu <lin2006yuo@gmail.com>
2019-12-17 23:05:59 +0800

    f

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const htmlparser_1 = __importDefault(require("./htmlparser"));
 const root = process.cwd();
-const html = fs_1.default.readFileSync(path_1.default.resolve(root, "src/index.vue"), "utf-8");
+const html = fs_1.default.readFileSync(path_1.default.resolve(root, "mock/template/index.vue"), "utf-8");
 const parser = new htmlparser_1.default();
 parser.parse(html).then(ast => {
     console.log(ast);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import fs from "fs"
-import path, { dirname } from "path"
+import path from "path"
 
 import htmlparser from "./htmlparser"
 
 const root = process.cwd()
-const html = fs.readFileSync(path.resolve(root,"src/index.vue"), "utf-8")
+const html = fs.readFileSync(path.resolve(root,"mock/template/index.vue"), "utf-8")
 const parser = new htmlparser()
 parser.parse(html).then(ast => {
   console.log(ast)
