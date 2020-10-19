04e835fedf232dfc141cd8689374ca7c84ed0ce1
linxueyu <lin2006yuo@gmail.com>
2020-10-19 14:16:57 +0800

    cli

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,2 @@
-import fs from 'fs'
-import path from 'path'
+#! /usr/bin/env node
 
-import htmlparser from './htmlparser'
-import { DomUtils } from 'htmlparser2'
-
-const root = process.cwd()
-const html = fs.readFileSync(
-  path.resolve(root, 'mock/template/index.vue'),
-  'utf-8'
-)
-const parser = new htmlparser()
-parser.parse(html).then(ast => {
-  console.log(ast)
-  debugger
-  let styleElem
-  for(let elem of ast) {
-    if(elem.tagName === 'style') styleElem = elem
-  }
-
-  console.log(parser.toString(styleElem))
-})
